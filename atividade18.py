90be0e950eed504ad09707c549bbbdfb28fd444e
erikedu10 <148131588+erikedu10@users.noreply.github.com>
2023-10-25 10:57:55 -0300

    Add files via upload

---
+++ atividade18.py
@@ -0,0 +1,7 @@
+numero = int(input("insira um número:"))
+numero2 = int(input("insira o segundo número:"))
+
+if numero > numero2:
+    print(f"o maio número é {numero}:")
+else:
+    print(f"o maior número é {numero2}:")
